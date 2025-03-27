ecb76bd9143c153e525f0b2afac81be41ebd5af2
Manish Kumar <mkc1020304050@gmail.com>
2025-03-27 09:51:30 +0530

    refactor: setters and getters added

--- src/main/java/com/ajackus/Digital/Library/Book/Management/System/dto/BookDTO.java
+++ src/main/java/com/ajackus/Digital/Library/Book/Management/System/dto/BookDTO.java
@@ -1,14 +1,5 @@
 package com.ajackus.Digital.Library.Book.Management.System.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@RequiredArgsConstructor
 // Data Transfer Object (DTO) for Book
 public class BookDTO {
     private String title;
@@ -16,4 +7,24 @@ public class BookDTO {
     private String genre;
     private String status;
 
+    public BookDTO() {}
+
+    public BookDTO(String title, String author, String genre, String status) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.status = status;
+    }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
--- src/main/java/com/ajackus/Digital/Library/Book/Management/System/entity/Book.java
+++ src/main/java/com/ajackus/Digital/Library/Book/Management/System/entity/Book.java
@@ -1,17 +1,9 @@
 package com.ajackus.Digital.Library.Book.Management.System.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 // Entity representing a Book
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@RequiredArgsConstructor
-@Data
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,10 +20,25 @@ public class Book {
     @Column(nullable = false)
     private String status;
 
+    // Constructors
+    public Book() {}
+
     public Book(String title, String author, String genre, String status) {
-        this.title=title;
-        this.author=author;
-        this.genre=genre;
-        this.status=status;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.status = status;
     }
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
